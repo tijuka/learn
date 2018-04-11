411af5b92420086347e2b388ea964f7bfe78627b
tijuka <you@example.com>
2018-04-11 21:03:37 +0300

    pridejau visus

---
+++ firstHW.php
@@ -0,0 +1,30 @@
+<?php
+
+$type = "cat";
+$name = "Tim";
+$age = 1;
+$weight = 3.6;
+
+if ($type == "cat" && $name == "Tim")
+echo "Tim is cat";
+else
+echo "Tim is not a cat!";
+
+?>
+
+<?php
+
+$price = 678;
+
+if ($price > 500 && $price < 600) {
+    echo "Price is big enough";
+}
+elseif ($price > 600 && $price < 700) {
+    echo "OMG";
+}
+else
+    echo "Price is ok";
+
+    ?>
+
+
---
+++ secondHW.php
@@ -0,0 +1,24 @@
+<?php
+function name()
+{
+
+    echo "<h1>Tija</h1>";
+}
+    name();
+
+?>
+
+<?php
+function boo ($type, boolean $heading)
+{
+
+
+    if ($heading== true)
+        echo "Heading";
+    else
+        echo $text;
+}
+boo($type, TRUE)
+
+?>
+
